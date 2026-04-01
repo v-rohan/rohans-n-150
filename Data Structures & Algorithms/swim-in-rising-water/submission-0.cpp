363061a009a186821c8c69039b9cb97447320d96
V Rohan Rao <rohanrao.dec11@gmail.com>
2026-04-02 00:46:54 +0530

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        priority_queue< pair<int,pair<int,int>>, vector< pair<int,pair<int,int>> >, greater< pair< int, pair<int,int> >> > pq;
+        pq.push({grid[0][0],{0,0}});
+
+        while(!pq.empty()){
+            int maxi = pq.top().first;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+
+            pq.pop();
+
+            grid[i][j] = -1;
+
+            if(i==grid.size()-1 && j==grid.size()-1){
+                return maxi;
+            }
+
+            else{
+                for(int k=-1;k<2;k++)
+                    for(int l=-1;l<2;l++)
+                        if((k==0||l==0) && i+k>=0 && i+k<grid.size() && j+l>=0 && j+l<grid.size() && grid[i+k][j+l]!=-1){
+                            pq.push({max(maxi,grid[i+k][j+l]),{i+k,j+l}});
+                        }
+            }
+
+
+        } 
+
+        return -1;
+    }
+};
