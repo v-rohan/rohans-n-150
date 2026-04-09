c647188f9ce1809215301a103c54a03f8bb59c29
V Rohan Rao <rohanrao.dec11@gmail.com>
2026-04-10 00:28:38 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses);
+
+        for(vector<int> t: prerequisites){
+            adj[t[1]].push_back(t[0]);
+            indegree[t[0]]++;
+        }
+
+        queue<int> bfsq;
+
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0) bfsq.push(i);
+        }
+
+        int finish = 0;
+
+        while(!bfsq.empty()){
+            int currIndex = bfsq.front();
+
+            bfsq.pop();
+
+            finish++;
+
+            for(int i: adj[currIndex]){
+                indegree[i]--;
+                if(indegree[i]==0) bfsq.push(i);
+            }
+            
+        }
+
+        return finish == numCourses;
+    }
+};
